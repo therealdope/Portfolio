049b153d4b46e2176d4992ffc183483f5f21aeb4
therealdope <skheni02@gmail.com>
2025-08-30 12:49:42 +0530

    .

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
       dark:text-white`}
       >
         {children}
-        <Chatbot/>
+        {/* <Chatbot/> */}
 
       </body>
     </html>
